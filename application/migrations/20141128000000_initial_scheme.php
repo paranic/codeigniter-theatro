82c94c664c75bcacecdc82d77a58f375761965d7
Παραστατίδης Νίκος <paranic@gmail.com>
2014-12-01 23:46:54 +0200

    Added troupes table to initial migration file. using initial migration file since we don't have any critical data to the database. and to keep daabase migration file tidy and single :-)

--- application/migrations/20141128000000_initial_scheme.php
+++ application/migrations/20141128000000_initial_scheme.php
@@ -7,7 +7,7 @@ class Migration_Initial_Scheme extends CI_Migration {
 	{
 		//$this->dbforge->create_database('theatro');
 		
-		/* users */
+		/* users table */
 		$fields = array(
 			'user_id' => array(
 				'type' => 'INT',
@@ -38,6 +38,26 @@ class Migration_Initial_Scheme extends CI_Migration {
 		$this->dbforge->add_field($fields);
 		$this->dbforge->add_key('user_id', TRUE);
 		$this->dbforge->create_table('users', TRUE);
+		
+		$this->db->insert('users', array('email'=>'default@user.com', 'password'=>'default'));
+		
+		/* troupes table */
+		$fields = array(
+			'troupe_id' => array(
+				'type' => 'INT',
+				'constraint' => 8,
+				'auto_increment' => TRUE
+			),
+			'troupe_name' => array(
+				'type' => 'VARCHAR',
+				'constraint' => 255,
+				'null' => TRUE,
+			)
+		);
+		$this->dbforge->add_field($fields);
+		$this->dbforge->add_key('troupe_id', TRUE);
+		$this->dbforge->create_table('troupes', TRUE);
+
 	}
 	
 	public function down()
